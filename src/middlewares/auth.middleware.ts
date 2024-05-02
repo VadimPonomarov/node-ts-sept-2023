9804d7ac71d19178b6b0084c6d105ec448105a77
Вадим <pvs.versia@gmail.com>
2024-05-02 21:48:14 +0300

    hw6

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -6,7 +6,7 @@ import { tokenRepository } from "../repositories/token.repository";
 import { jwtService } from "../services/jwt.service";
 
 class AuthMiddleware {
-  public isAuth(req: Request, res: Response, next: NextFunction) {
+  public async isAuth(req: Request, res: Response, next: NextFunction) {
     try {
       if (!req.headers.authorization) throw new ApiError("Not authorised", 401);
       const [bearer, token] = req.headers.authorization
@@ -14,12 +14,15 @@ class AuthMiddleware {
         .trim()
         .split(/\s+/);
       if (bearer.toLowerCase() !== "bearer" || !token) {
-        throw new ApiError("Not authorised", 401);
+        throw new ApiError(
+          "You are supposed to provide JWT as: Bearer + JWT",
+          401,
+        );
       }
-      if (!tokenRepository.getOne(token))
-        throw new ApiError("Wrong token type", 401);
+      const isRegistered = await tokenRepository.getOne(token);
+      if (!isRegistered) throw new ApiError("Token is not registered yet", 401);
       const { type } = jwtService.isJwtValid(token) as IJwtPayload;
-      if (type !== "access") throw new ApiError("Wrong jwt type", 401);
+      if (type !== "access") throw new ApiError("Wrong JWT type", 401);
       next();
     } catch (e) {
       next(e);

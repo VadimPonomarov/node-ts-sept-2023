c27004350a11620f781581cac19c5370b38d332b
Вадим <pvs.versia@gmail.com>
2024-05-03 18:42:06 +0300

    hw6

--- src/common/configs/index.ts
+++ src/common/configs/index.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  PORT: Number(process.env.PORT) || 3001,
+  PORT: Number(process.env.PORT) || 3000,
   HOST: process.env.HOST,
   MONGO_URL: process.env.MONGO_URL,
   JWT_SECRET: process.env.JWT_SECRET,
